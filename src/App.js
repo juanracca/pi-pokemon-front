dbfa3c885d7d8d9a0fdd8442729386fbcc5ecae0
Juan Racca <juanracca_@hotmail.com>
2022-07-26 11:14:50 -0300

    base del proyecto creada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import LandingPage from './components/LandingPage';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
+import Detail from './components/Detail';
+import PokemonCreate from './components/PokemonCreate';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <Routes>
+        <Route exact path = '/' element = {<LandingPage/>}/>
+        <Route path = '/pokemons' element = {<Home/>}/>
+        <Route path = '/pokemons/:id' element = {<Detail/>}/>
+        <Route path = '/create' element = {<PokemonCreate/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LandingPage(){
+    return(
+        <div background-color='black'>
+            <h1>POKEMON</h1>
+             <button>ENTER</button>
+        </div>
+    );
+};
